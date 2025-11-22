a21b8cc05c7ef98d3f842f155b76e4b181083d43
chandni-ahuja-04 <85622993+chandni-ahuja-04@users.noreply.github.com>
2025-11-22 14:42:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
