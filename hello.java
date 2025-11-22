296df1eac787b27770dafbe068db35e51e8db170
chandni-ahuja-04 <85622993+chandni-ahuja-04@users.noreply.github.com>
2025-11-22 15:45:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,21 @@
-public class hello {
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+
+public class Hello {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Thread.sleep(Long.MAX_VALUE);
+        HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
+
+        server.createContext("/", (exchange -> {
+            String response = "Hello from Java HTTP server!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }));
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server running on port 8080");
     }
 }
